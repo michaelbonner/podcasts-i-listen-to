7b5c21fd6a104b395428141e8801ea3b9fcd7b9a
Michael Bonner <michaelwbonner@gmail.com>
2019-07-26 00:22:01 -0400

    styling

--- pages/index.js
+++ pages/index.js
@@ -12,31 +12,39 @@ function Home({ podcasts }) {
         <link rel="icon" href="/static/favicon.ico" />
         <meta name="viewport" content="width=device-width" />
       </Helmet>
-      <div className="bg-orange-400 text-white py-8 mb-8">
+      <div className="bg-indigo-900 shadow-lg text-white py-12">
         <div className="container mx-auto">
-          <h1 className="text-center text-4xl">Podcasts I Listen To</h1>
+          <h1 className="text-center text-5xl">The Podcasts I Listens To</h1>
         </div>
       </div>
-      <div className="flex flex-wrap container mx-auto items-start">
-        {podcasts.map(podcast => {
-          return (
-            <a
-              key={podcast.title}
-              className="w-full lg:w-1/2 lg:w-1/6 p-4 text-center"
-              href={podcast.url}
-              target="_blank"
-            >
-              <img
-                className="mx-auto my-4 rounded shadow"
-                alt={`${podcast.title} Poster`}
-                src={podcast.image}
-              />
-              <div dangerouslySetInnerHTML={{ __html: podcast.title }} />
-            </a>
-          );
-        })}
+      <div className="bg-gray-200 py-16">
+        <div className="flex flex-wrap container mx-auto items-stretch">
+          {podcasts.map(podcast => {
+            return (
+              <div
+                key={podcast.title}
+                className="w-full h-full md:w-1/2 p-4"
+              >
+                <div className="flex bg-white items-center rounded-lg shadow-lg w-full">
+                  <a href={podcast.url} target="_blank" className="w-1/3">
+                    <img
+                      className="rounded-lg rounded-r-none"
+                      alt={`${podcast.title} Poster`}
+                      src={podcast.image}
+                    />
+                  </a>
+                  <p className="w-2/3 pl-4 text-xl font-semibold">
+                    <a href={podcast.url} target="_blank">
+                      {podcast.title}
+                    </a>
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className="bg-gray-200 py-6">
+      <div className="bg-gray-800 text-white pt-16 pb-4">
         <div className="text-center container mx-auto">
           <h2 className="text-2xl font-semibold">
             Check out Overcast, it's free
@@ -48,10 +56,10 @@ function Home({ podcasts }) {
               alt="overcast"
             />
           </a>
-          <h2 className="text-xl">
+          <h2 className="text-2xl font-semibold pt-4">
             Overcast<sup>®</sup>
           </h2>
-          <p>
+          <p className="text-gray-500 pb-4">
             A powerful yet simple podcast player for iPhone, iPad, and Apple
             Watch.
           </p>
@@ -63,6 +71,13 @@ function Home({ podcasts }) {
               alt="Download"
             />
           </a>
+
+          <p className="pt-16 text-gray-300">
+            &copy; {new Date().getFullYear()} <a href="https://michaelbonner.dev/">Michael Bonner</a>
+            <span className="px-4">&ndash;</span>
+            <a className="pr-4" href="https://github.com/michaelbonner">GitHub</a>
+            <a href="https://www.instagram.com/mbonner4/">Instagram</a>
+          </p>
         </div>
       </div>
     </>
