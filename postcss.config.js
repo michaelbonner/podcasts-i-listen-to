df32a35fc43b8e23ef8868b88254799c580c5e8e
Michael Bonner <michaelwbonner@gmail.com>
2019-07-27 09:52:15 -0400

    add layouts to purgecss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ["./pages/*.js"],
+  content: [
+    "./pages/*.js",
+    "./layouts/*.js"
+  ],
   whitelist: ['body'],
 
   // Include any special characters you're using in this regular expression
