24ebef6d2f5c74bb0bc80e8246afd59303658a99
Michael Bonner <michaelwbonner@gmail.com>
2019-07-31 15:04:30 -0600

    add gtm

--- layouts/main.js
+++ layouts/main.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 import "../styles/index.css";
 
 function MainLayout({ children }) {
@@ -10,6 +10,23 @@ function MainLayout({ children }) {
         <link rel="canonical" href="https://podcasts.michaelbonner.dev" />
         <link rel="icon" href="/static/favicon.ico" />
         <meta name="viewport" content="width=device-width" />
+
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WKV6C4B');`
+            }}
+          />
+        </Head>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WKV6C4BX" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`
+          }}
+        />
       </Head>
       <div className="bg-indigo-800 text-indigo-100 py-12">
         <div className="container mx-auto flex flex-wrap items-center justify-center">
