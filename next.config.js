6f882255f202d18e69c4b78c008e9df288f13369
Michael Bonner <michaelwbonner@gmail.com>
2019-08-04 22:36:44 -0600

    only load .env if file exists in next.config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
-const dotEnvResult = require("dotenv").config();
+const path = __dirname + '/.env';
+if (fs.existsSync(path)) {
+  const dotEnvResult = require("dotenv").config();
 
-if (dotEnvResult.error) {
-  throw dotEnvResult.error;
-}
-
-module.exports = {
-  env: {
-    FUNCTIONS_HOST: process.env.FUNCTIONS_HOST,
-    MONGO_DB_USER: process.env.MONGO_DB_USER,
-    MONGO_DB_PASSWORD: process.env.MONGO_DB_PASSWORD,
+  if (dotEnvResult.error) {
+    throw dotEnvResult.error;
   }
-};
+
+  module.exports = {
+    env: {
+      FUNCTIONS_HOST: process.env.FUNCTIONS_HOST,
+      MONGO_DB_USER: process.env.MONGO_DB_USER,
+      MONGO_DB_PASSWORD: process.env.MONGO_DB_PASSWORD
+    }
+  };
+}
 
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS({});
