ccff61be9c421b48b30ff7c08204bc51f1aa694c
Michael Bonner <michaelwbonner@gmail.com>
2019-08-04 22:57:15 -0600

    override netlify values if .env exists

--- functions/podcasts.js
+++ functions/podcasts.js
@@ -1,17 +1,23 @@
 const mongoose = require("mongoose");
 const fs = require("fs");
 
-const path = __dirname + '/../.env';
+const path = __dirname + "/../.env";
 if (fs.existsSync(path)) {
-    const dotEnvResult = require("dotenv").config({ path: path });
-    
-    if (dotEnvResult.error) {
-      throw dotEnvResult.error;
-    }
+  const dotEnvResult = require("dotenv").config({ path: path });
+
+  if (dotEnvResult.error) {
+    throw dotEnvResult.error;
+  }
+
+  process.env.FUNCTIONS_HOST = dotEnvResult.parsed.FUNCTIONS_HOST;
+  process.env.MONGO_DB_USER = dotEnvResult.parsed.MONGO_DB_USER;
+  process.env.MONGO_DB_PASSWORD = dotEnvResult.parsed.MONGO_DB_PASSWORD;
 }
 
 const password = encodeURIComponent(process.env.MONGO_DB_PASSWORD);
-const uri = `mongodb+srv://${process.env.MONGO_DB_USER}:${password}@cluster0-urfp2.mongodb.net/podcastsilistento?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${
+  process.env.MONGO_DB_USER
+}:${password}@cluster0-urfp2.mongodb.net/podcastsilistento?retryWrites=true&w=majority`;
 
 const db = mongoose.connect(uri, {
   useNewUrlParser: true
@@ -20,7 +26,7 @@ const db = mongoose.connect(uri, {
 const Podcast = require("./podcastModel");
 
 exports.handler = async (event, context) => {
-  const podcasts = await Podcast.find({}).sort('title');
+  const podcasts = await Podcast.find({}).sort("title");
 
   return {
     statusCode: 200,
--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
-const path = __dirname + '/.env';
+const path = __dirname + "/.env";
 if (fs.existsSync(path)) {
   const dotEnvResult = require("dotenv").config();
 
   if (dotEnvResult.error) {
     throw dotEnvResult.error;
   }
+
+  process.env.FUNCTIONS_HOST = dotEnvResult.parsed.FUNCTIONS_HOST;
+  process.env.MONGO_DB_USER = dotEnvResult.parsed.MONGO_DB_USER;
+  process.env.MONGO_DB_PASSWORD = dotEnvResult.parsed.MONGO_DB_PASSWORD;
 
   module.exports = {
     env: {
