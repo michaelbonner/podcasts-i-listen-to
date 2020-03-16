1f0cd4a4d213e41225c47c47f203abba6ad679b5
Michael Bonner <michaelwbonner@gmail.com>
2020-03-15 18:06:39 -0600

    add star filter

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,97 @@
 import fetch from "isomorphic-unfetch";
 import MainLayout from "../layouts/main";
+import { useState, useEffect } from "react";
 
 const PORT = process.env.PORT || 3000;
 
+const Star = ({ filled }) => {
+  return (
+    <svg
+      className={`block w-6 h-6 ${
+        filled ? "text-yellow-400" : "text-gray-300"
+      }`}
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+    >
+      <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+    </svg>
+  );
+};
+
 function Home({ podcasts }) {
+  const [filter, setFilter] = useState(0);
+  const [filteredPodcasts, setFilteredPodcasts] = useState(podcasts);
+
+  useEffect(() => {
+    setFilteredPodcasts(podcasts.filter(podcast => podcast.rating >= filter));
+  }, [filter]);
+
   return (
     <MainLayout>
-      <div className="bg-indigo-100 py-16">
-        <div className="flex flex-wrap container mx-auto items-stretch">
-          {podcasts.map(podcast => {
+      <div className="bg-indigo-100 pt-8 pb-16">
+        <div className="flex flex-wrap container mx-auto items-center justify-end px-2 text-yellow-500">
+          <button
+            type="button"
+            onClick={() => setFilter(0)}
+            className={`flex flex-wrap mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none ${
+              filter === 0 ? "bg-yellow-200" : "bg-white"
+            } `}
+          >
+            <strong>All</strong>
+          </button>
+          <button
+            type="button"
+            onClick={() => setFilter(2)}
+            className={`flex flex-wrap mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none ${
+              filter === 2 ? "bg-yellow-200" : "bg-white"
+            } `}
+          >
+            <Star filled={true} />
+            <Star filled={true} />
+            <span className="font-black pl-1">+</span>
+          </button>
+          <button
+            type="button"
+            onClick={() => setFilter(3)}
+            className={`flex flex-wrap mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none ${
+              filter === 3 ? "bg-yellow-200" : "bg-white"
+            } `}
+          >
+            <Star filled={true} />
+            <Star filled={true} />
+            <Star filled={true} />
+            <span className="font-black pl-1">+</span>
+          </button>
+          <button
+            type="button"
+            onClick={() => setFilter(4)}
+            className={`flex flex-wrap mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none ${
+              filter === 4 ? "bg-yellow-200" : "bg-white"
+            } `}
+          >
+            <Star filled={true} />
+            <Star filled={true} />
+            <Star filled={true} />
+            <Star filled={true} />
+            <span className="font-black pl-1">+</span>
+          </button>
+          <button
+            type="button"
+            onClick={() => setFilter(5)}
+            className={`flex flex-wrap mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none ${
+              filter === 5 ? "bg-yellow-200" : "bg-white"
+            } `}
+          >
+            <Star filled={true} />
+            <Star filled={true} />
+            <Star filled={true} />
+            <Star filled={true} />
+            <Star filled={true} />
+          </button>
+        </div>
+        <div className="flex flex-wrap container mx-auto mt-4 items-stretch">
+          {filteredPodcasts.map(podcast => {
             return (
               <div key={podcast.title} className="w-full h-full md:w-1/2 p-4">
                 <div className="flex bg-white items-center rounded-lg shadow-lg w-full">
@@ -28,66 +111,11 @@ function Home({ podcasts }) {
                       </a>
                     </p>
                     <p className="w-1/2 flex my-2">
-                      <svg
-                        className={`block w-6 h-6 ${
-                          podcast.rating > 0
-                            ? "text-yellow-400"
-                            : "text-gray-300"
-                        }`}
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                      </svg>
-                      <svg
-                        className={`block w-6 h-6 ${
-                          podcast.rating > 1
-                            ? "text-yellow-400"
-                            : "text-gray-300"
-                        }`}
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                      </svg>
-                      <svg
-                        className={`block w-6 h-6 ${
-                          podcast.rating > 2
-                            ? "text-yellow-400"
-                            : "text-gray-300"
-                        }`}
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                      </svg>
-                      <svg
-                        className={`block w-6 h-6 ${
-                          podcast.rating > 3
-                            ? "text-yellow-400"
-                            : "text-gray-300"
-                        }`}
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                      </svg>
-                      <svg
-                        className={`block w-6 h-6 ${
-                          podcast.rating > 4
-                            ? "text-yellow-400"
-                            : "text-gray-300"
-                        }`}
-                        fill="currentColor"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                      </svg>
+                      <Star filled={podcast.rating > 0} />
+                      <Star filled={podcast.rating > 1} />
+                      <Star filled={podcast.rating > 2} />
+                      <Star filled={podcast.rating > 3} />
+                      <Star filled={podcast.rating > 4} />
                     </p>
                   </div>
                 </div>
