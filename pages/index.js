2adf3c8bcac86f1533f7f7bb44ec558433a4b261
Michael Bonner <michaelwbonner@gmail.com>
2020-04-24 12:31:56 -0600

    default hide filters

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,7 @@ function Home() {
   const [sortedPodcasts, setSortedPodcasts] = useState([]);
   const [filteredPodcasts, setFilteredPodcasts] = useState([]);
   const [tags, setTags] = useState([]);
+  const [toggleFilters, setToggleFilters] = useState(true);
 
   useEffect(() => {
     setSortedPodcasts(
@@ -61,85 +62,128 @@ function Home() {
   return (
     <MainLayout>
       <div className="bg-indigo-100 pt-8 pb-16">
-        <div className="flex flex-wrap container mx-auto items-center justify-end px-2 text-yellow-500 text-sm">
+        {/* Filters */}
+        <div className="container mx-auto px-4 text-right flex justify-end">
           <button
-            type="button"
-            onClick={() => setRatingFilter(0)}
-            className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none font-bold ${
-              ratingFilter === 0 ? "bg-yellow-200" : "bg-white"
-            } `}
+            className="flex items-center bg-white rounded py-2 px-4 shadow-md focus:outline-none focus:bg-gray-300 font-semibold text-gray-600 text-sm"
+            onClick={() => setToggleFilters(!toggleFilters)}
           >
-            All
-          </button>
-          <button
-            type="button"
-            onClick={() => setRatingFilter(3)}
-            className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none ${
-              ratingFilter === 3 ? "bg-yellow-200" : "bg-white"
-            } `}
-          >
-            <Star filled={true} />
-            <Star filled={true} />
-            <Star filled={true} />
-            <span className="font-black pl-1">+</span>
-          </button>
-          <button
-            type="button"
-            onClick={() => setRatingFilter(4)}
-            className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none ${
-              ratingFilter === 4 ? "bg-yellow-200" : "bg-white"
-            } `}
-          >
-            <Star filled={true} />
-            <Star filled={true} />
-            <Star filled={true} />
-            <Star filled={true} />
-            <span className="font-black pl-1">+</span>
-          </button>
-          <button
-            type="button"
-            onClick={() => setRatingFilter(5)}
-            className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none ${
-              ratingFilter === 5 ? "bg-yellow-200" : "bg-white"
-            } `}
-          >
-            <Star filled={true} />
-            <Star filled={true} />
-            <Star filled={true} />
-            <Star filled={true} />
-            <Star filled={true} />
-          </button>
-        </div>
-        <div className="flex flex-wrap container mx-auto items-center justify-end px-2 text-yellow-500 text-sm">
-          <button
-            type="button"
-            onClick={() => setTagFilter("")}
-            className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none font-bold ${
-              tagFilter === "" ? "bg-yellow-200" : "bg-white"
-            } `}
-          >
-            All
-          </button>
-          {tags.map((tag) => (
-            <button
-              className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none font-bold ${
-                tagFilter === tag ? "bg-yellow-200" : "bg-white"
-              } `}
-              key={tag}
-              onClick={() => setTagFilter(tag)}
-              type="button"
+            Filters
+            <svg
+              className="w-5 h-5 ml-2 text-indigo-600 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              width="512"
+              height="512"
+              viewBox="0 0 512 512"
             >
-              {tag}
-            </button>
-          ))}
+              <path d="M472,168H40a24,24,0,0,1,0-48H472a24,24,0,0,1,0,48Z" />
+              <path d="M392,280H120a24,24,0,0,1,0-48H392a24,24,0,0,1,0,48Z" />
+              <path d="M296,392H216a24,24,0,0,1,0-48h80a24,24,0,0,1,0,48Z" />
+            </svg>
+          </button>
         </div>
+        {toggleFilters && (
+          <div className="flex flex-wrap container mx-auto justify-between">
+            <div className="w-full lg:w-1/2">
+              <h3 className="font-semibold text-lg text-indigo-800 mt-6 lg:mt-0 mx-4">
+                Rating
+              </h3>
+              <div className="flex flex-wrap items-center px-2 text-yellow-500 text-sm">
+                <button
+                  type="button"
+                  onClick={() => setRatingFilter(0)}
+                  className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none font-bold ${
+                    ratingFilter === 0 ? "bg-yellow-200" : "bg-white"
+                  } `}
+                >
+                  All
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setRatingFilter(3)}
+                  className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none ${
+                    ratingFilter === 3 ? "bg-yellow-200" : "bg-white"
+                  } `}
+                >
+                  <Star filled={true} />
+                  <Star filled={true} />
+                  <Star filled={true} />
+                  <span className="font-black pl-1">+</span>
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setRatingFilter(4)}
+                  className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none ${
+                    ratingFilter === 4 ? "bg-yellow-200" : "bg-white"
+                  } `}
+                >
+                  <Star filled={true} />
+                  <Star filled={true} />
+                  <Star filled={true} />
+                  <Star filled={true} />
+                  <span className="font-black pl-1">+</span>
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setRatingFilter(5)}
+                  className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none ${
+                    ratingFilter === 5 ? "bg-yellow-200" : "bg-white"
+                  } `}
+                >
+                  <Star filled={true} />
+                  <Star filled={true} />
+                  <Star filled={true} />
+                  <Star filled={true} />
+                  <Star filled={true} />
+                </button>
+              </div>
+            </div>
+            <div className="w-full lg:w-1/2">
+              <h3 className="font-semibold text-lg text-indigo-800 mt-6 lg:mt-0 mx-4">
+                Tags
+              </h3>
+              <div className="flex flex-wrap items-center lg:justify-end lg:mt-0 px-2 text-yellow-500 text-sm">
+                <button
+                  type="button"
+                  onClick={() => setTagFilter("")}
+                  className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none font-bold ${
+                    tagFilter === "" ? "bg-yellow-200" : "bg-white"
+                  } `}
+                >
+                  All
+                </button>
+                {tags.map((tag) => (
+                  <button
+                    className={`flex flex-wrap items-center mt-4 mx-2 py-2 px-4 rounded shadow-md focus:outline-none font-bold ${
+                      tagFilter === tag ? "bg-yellow-200" : "bg-white"
+                    } `}
+                    key={tag}
+                    onClick={() => setTagFilter(tag)}
+                    type="button"
+                  >
+                    {tag}
+                  </button>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+        {/* End filters */}
+
         <div className="flex flex-wrap container mx-auto mt-4 items-stretch">
           {filteredPodcasts.length ? (
             filteredPodcasts.map((podcast) => {
               return (
-                <div key={podcast.title} className="w-full h-full md:w-1/2 p-4">
+                <div
+                  key={podcast.title}
+                  className="w-full h-full md:w-1/2 p-4 overflow-x-hidden"
+                >
                   <div className="flex bg-white items-center rounded-lg shadow-lg w-full">
-                    <a href={podcast.url} target="_blank" className="w-1/4">
+                    <a
+                      href={podcast.url}
+                      target="_blank"
+                      className="w-2/5 lg:w-1/4"
+                    >
                       <LazyLoadImage
                         className="rounded-lg rounded-r-none w-full"
                         alt={`${podcast.title} Poster`}
@@ -148,9 +192,9 @@ function Home() {
                         width={400}
                       />
                     </a>
-                    <div className="w-3/4 px-6 flex flex-col justify-between">
+                    <div className="w-3/5 lg:w-3/4 px-6 flex flex-col justify-between">
                       <div>
-                        <p className="text-xl font-semibold">
+                        <p className="text-xl font-semibold truncate">
                           <a href={podcast.url} target="_blank">
                             <span
                               dangerouslySetInnerHTML={{
@@ -159,7 +203,7 @@ function Home() {
                             />
                           </a>
                         </p>
-                        <div className="my-2">
+                        <div className="my-1">
                           {podcast.tags.map((tag) => (
                             <button
                               className="inline-block bg-gray-200 rounded-full px-3 py-1 text-xs font-semibold text-gray-700 mr-2 focus:outline-none focus:bg-gray-300"
