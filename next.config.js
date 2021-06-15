8f85825a995a8057ffbcff8214da968665d1ff09
Michael Bonner <michaelwbonner@gmail.com>
2021-06-15 12:33:57 -0600

    update to next js 11

--- next.config.js
+++ next.config.js
@@ -9,14 +9,4 @@ module.exports = {
   images: {
     domains: ["overcast.fm"],
   },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
-
-    return config;
-  },
 };
