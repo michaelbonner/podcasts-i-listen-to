a48f5afa5db7be24615c80541deff5b217343814
Michael Bonner <michaelwbonner@gmail.com>
2021-09-20 12:20:47 -0600

    adjust grid gap

--- pages/index.js
+++ pages/index.js
@@ -378,74 +378,72 @@ function Home({ podcasts }) {
         )}
         {/* End filters */}
 
-        <div className="mx-auto xl:mx-12 2xl:mx-24 3xl:mx-36 4xl:mx-72 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-4">
+        <div className="lg:mx-auto xl:mx-12 2xl:mx-24 3xl:mx-36 4xl:mx-72 px-4 grid gap-4 xl:gap-6 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 mt-4">
           {filteredPodcasts.length ? (
             filteredPodcasts.map((podcast) => {
               return (
                 <div
                   key={podcast.itunesId}
-                  className="w-full h-full p-4 overflow-x-hidden"
+                  className="flex bg-white items-center rounded-lg shadow-lg w-full"
                 >
-                  <div className="flex bg-white items-center rounded-lg shadow-lg w-full">
-                    <a
-                      className="w-2/5 lg:w-1/4 flex items-center"
-                      href={podcast.itunesData?.collectionViewUrl}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      title={`${podcast.itunesData?.collectionName} Poster`}
-                    >
-                      <Image
-                        className="rounded-lg rounded-r-none w-full h-full"
-                        alt={`${podcast.itunesData?.collectionName} Poster`}
-                        height={200}
-                        src={podcast.itunesData?.artworkUrl600}
-                        width={200}
-                      />
-                    </a>
-                    <div className="w-3/5 lg:w-3/4 px-6 flex flex-col justify-between">
-                      <div>
-                        <p className="text-xl font-bold truncate pt-3">
-                          <a
-                            href={podcast.itunesData?.collectionViewUrl}
-                            rel="noopener noreferrer"
-                            target="_blank"
+                  <a
+                    className="w-2/5 lg:w-1/4 flex items-center"
+                    href={podcast.itunesData?.collectionViewUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title={`${podcast.itunesData?.collectionName} Poster`}
+                  >
+                    <Image
+                      className="rounded-lg rounded-r-none w-full h-full"
+                      alt={`${podcast.itunesData?.collectionName} Poster`}
+                      height={200}
+                      src={podcast.itunesData?.artworkUrl600}
+                      width={200}
+                    />
+                  </a>
+                  <div className="w-3/5 lg:w-3/4 px-6 flex flex-col justify-between">
+                    <div>
+                      <p className="text-xl font-bold truncate pt-3">
+                        <a
+                          href={podcast.itunesData?.collectionViewUrl}
+                          rel="noopener noreferrer"
+                          target="_blank"
+                        >
+                          <span
+                            dangerouslySetInnerHTML={{
+                              __html: podcast.itunesData?.collectionName,
+                            }}
+                          />
+                        </a>
+                      </p>
+                    </div>
+                    <div className="lg:flex lg:flex-row-reverse justify-between">
+                      <p className="flex lg:justify-end my-2">
+                        <Star filled={podcast.rating > 0} />
+                        <Star filled={podcast.rating > 1} />
+                        <Star filled={podcast.rating > 2} />
+                        <Star filled={podcast.rating > 3} />
+                        <Star filled={podcast.rating > 4} />
+                      </p>
+                      <div className="flex flex-wrap justify-start space-x-2 mt-4 lg:my-2">
+                        {podcast.tags.map((tag) => (
+                          <button
+                            className={`inline-block ${
+                              tagFilter === tag
+                                ? "bg-green-200 text-green-700"
+                                : "bg-gray-200 text-gray-700"
+                            } rounded px-2 py-1 text-xs font-semibold focus:outline-none`}
+                            key={tag}
+                            onClick={() =>
+                              tagFilter === tag
+                                ? setTagFilter("")
+                                : setTagFilter(tag)
+                            }
+                            type="button"
                           >
-                            <span
-                              dangerouslySetInnerHTML={{
-                                __html: podcast.itunesData?.collectionName,
-                              }}
-                            />
-                          </a>
-                        </p>
-                      </div>
-                      <div className="lg:flex lg:flex-row-reverse justify-between">
-                        <p className="flex lg:justify-end my-2">
-                          <Star filled={podcast.rating > 0} />
-                          <Star filled={podcast.rating > 1} />
-                          <Star filled={podcast.rating > 2} />
-                          <Star filled={podcast.rating > 3} />
-                          <Star filled={podcast.rating > 4} />
-                        </p>
-                        <div className="flex justify-start space-x-2 mt-4 lg:my-2">
-                          {podcast.tags.map((tag) => (
-                            <button
-                              className={`inline-block ${
-                                tagFilter === tag
-                                  ? "bg-green-200 text-green-700"
-                                  : "bg-gray-200 text-gray-700"
-                              } rounded px-2 py-1 text-xs font-semibold focus:outline-none`}
-                              key={tag}
-                              onClick={() =>
-                                tagFilter === tag
-                                  ? setTagFilter("")
-                                  : setTagFilter(tag)
-                              }
-                              type="button"
-                            >
-                              {tag}
-                            </button>
-                          ))}
-                        </div>
+                            {tag}
+                          </button>
+                        ))}
                       </div>
                     </div>
                   </div>
