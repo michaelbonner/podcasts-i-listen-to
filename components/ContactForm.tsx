72211e483a28f10fc8b051da5f39269d39cc88ff
Michael Bonner <michaelwbonner@gmail.com>
2024-09-25 10:25:58 -0600

    more styling

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -5,9 +5,12 @@ const ContactForm = () => {
   const [submitState, setSubmitState] = useState("initial");
   const [errorMessage, setErrorMessage] = useState("");
 
-  const [yourName, setYourName] = useState("");
-  const [podcastName, setPodcastName] = useState("");
-  const [podcastUrl, setPodcastUrl] = useState("");
+  const [formData, setFormData] = useState({
+    your_name: "",
+    podcast_name: "",
+    podcast_url: "",
+  });
+
   const [token, setToken] = useState("");
 
   const onSubmit = async (e: any) => {
@@ -22,22 +25,24 @@ const ContactForm = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          your_name: yourName,
-          podcast_name: podcastName,
-          podcast_url: podcastUrl,
+          ...formData,
           token,
         }),
       });
       const response = await submit.json();
       if (response.success) {
+        setFormData({
+          your_name: "",
+          podcast_name: "",
+          podcast_url: "",
+        });
         setSubmitState("submitted");
-        setPodcastName("");
-        setPodcastUrl("");
       } else {
         setErrorMessage(response.data);
         setSubmitState("error");
       }
     } catch (error) {
+      console.error("submit error", error);
       setErrorMessage(
         "There was a problem connecting to the API. Please let me know so I can get it fixed."
       );
@@ -59,43 +64,55 @@ const ContactForm = () => {
           <p>{errorMessage}</p>
         </div>
       )}
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full px-3 mt-6">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="your-name"
-          >
-            Your Name
-          </label>
-          <input
-            autoComplete="name"
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="your-name"
-            name="your_name"
-            onChange={(e) => setYourName(e.target.value)}
-            placeholder=""
-            required
-            type="text"
-            value={yourName}
-          />
-        </div>
-        <div className="w-full px-3 mt-6">
-          <label
-            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="podcast-name"
-          >
-            Podcast Name
-          </label>
-          <input
-            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="podcast-name"
-            name="podcast_name"
-            onChange={(e) => setPodcastName(e.target.value)}
-            placeholder=""
-            required
-            type="text"
-            value={podcastName}
-          />
+      <div className="flex flex-wrap -mx-3 mb-6 mt-2 px-2 lg:pl-8 lg:pr-4">
+        <div className="w-full grid lg:grid-cols-2 gap-2">
+          <div className="w-full px-3 mt-6">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="your-name"
+            >
+              Your Name
+            </label>
+            <input
+              autoComplete="name"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="your-name"
+              name="your_name"
+              onChange={(e) =>
+                setFormData((previous) => ({
+                  ...previous,
+                  your_name: e.target.value,
+                }))
+              }
+              placeholder=""
+              required
+              type="text"
+              value={formData.your_name}
+            />
+          </div>
+          <div className="w-full px-3 mt-6">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="podcast-name"
+            >
+              Podcast Name
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="podcast-name"
+              name="podcast_name"
+              onChange={(e) =>
+                setFormData((previous) => ({
+                  ...previous,
+                  podcast_name: e.target.value,
+                }))
+              }
+              placeholder=""
+              required
+              type="text"
+              value={formData.podcast_name}
+            />
+          </div>
         </div>
         <div className="w-full px-3 mt-6">
           <label
@@ -108,31 +125,34 @@ const ContactForm = () => {
             className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="podcast-url"
             name="podcast_url"
-            onChange={(e) => setPodcastUrl(e.target.value)}
+            onChange={(e) =>
+              setFormData((previous) => ({
+                ...previous,
+                podcast_url: e.target.value,
+              }))
+            }
             placeholder="https://"
             required
             type="url"
-            value={podcastUrl}
+            value={formData.podcast_url}
           />
         </div>
         {submitState === "submitted" && (
-          <div className="my-4 pt-6 border-t">
+          <div className="my-4 pt-6 border-t w-full px-4">
             <h3 className="text-sky-700 font-bold text-xl">
               Thank you for your recommendation!
             </h3>
           </div>
         )}
-        <div className="w-full flex flex-wrap justify-end items-end px-3 mt-8 gap-8">
-          <div>
-            <Turnstile
-              siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
-              onSuccess={(token) => setToken(token)}
-              options={{
-                theme: "light",
-              }}
-            />
-          </div>
-          <div>
+        <div className="w-full flex flex-wrap justify-between items-end px-3 mt-8 gap-8">
+          <Turnstile
+            siteKey={process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY}
+            onSuccess={(token) => setToken(token)}
+            options={{
+              theme: "light",
+            }}
+          />
+          <div className="pb-1.5">
             <button
               disabled={submitState === "submitting" || !token}
               className={`shadow bg-sky-600 hover:bg-sky-500 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded transition-all ${
@@ -140,7 +160,7 @@ const ContactForm = () => {
               }`}
               type="submit"
             >
-              Send it
+              Submit
             </button>
           </div>
         </div>
