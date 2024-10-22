939f368a37d48804534dfcdd529fb7b9c9267d24
Michael Bonner <michaelwbonner@gmail.com>
2024-10-22 12:11:02 -0600

    remove unused props

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-  swcMinify: true,
   env: {
     SLACK_WEBHOOK_URL: process.env.SLACK_WEBHOOK_URL,
     PODCASTS_FAUNA_DB_SECRET: process.env.PODCASTS_FAUNA_DB_SECRET,
